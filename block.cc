337aaf161d93d72d905cd1d7d061e65ca2559494
yonde927 <svyuta1202@gmail.com>
2022-08-15 06:52:55 +0900

    super_block

--- block.cc
+++ block.cc
@@ -9,8 +9,35 @@ namespace fique{
         file_stream->read((char*)&prev_offset, sizeof(int));
         file_stream->read((char*)&next_offset, sizeof(int));
     }
+    
+    block::block(shared_ptr<fstream> _file_stream, int _offset, int _block_size, int _prev_offset, int _next_offset){
+        file_stream = _file_stream;
+        this_offset = _offset;
+        block_size = _block_size;
+        prev_offset = _prev_offset;
+        next_offset = _next_offset;
+    }
 
     shared_ptr<block> block::prev_block(){
         return std::make_shared<block>(file_stream, prev_offset, block_size);
     }
+
+    shared_ptr<block> block::next_block(){
+        return std::make_shared<block>(file_stream, prev_offset, block_size);
+    }
+
+    void block::delete_block(){
+        file_stream->seekg(prev_offset + sizeof(int));
+        file_stream->write((const char*)&next_offset, sizeof(int));
+    }
+
+    super_block::super_block(shared_ptr<fstream> _file_stream, int _block_size): block(_file_stream, 0, _block_size, -1, -1){
+        block_num = 0; 
+        first_empty_block_offset = block_size; 
+        first_used_block_offset = -1; 
+        final_used_block_offset = -1;
+        //ファイルへの書き込み
+    }
+
+    shared_ptr<block> super_block::new_block(){}
 }
--- block.h
+++ block.h
@@ -2,10 +2,11 @@
 #include <memory>
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <cstdio>
 #include <unistd.h>
 
-using std::shared_ptr, std::fstream, std::vector;
+using std::shared_ptr, std::fstream, std::vector, std::string;
 
 namespace fique{
     //block
@@ -18,17 +19,23 @@ namespace fique{
         vector<char> buffer;
         shared_ptr<fstream> file_stream;
 
+        //既存ブロックからの初期化
         block(shared_ptr<fstream> _fs, int _offset, int _block_size);
+        //新規ブロックからの初期化
+        block(shared_ptr<fstream> _fs, int _offset, int _block_size, int _prev_offset, int _next_offset);
         shared_ptr<block> prev_block();
         shared_ptr<block> next_block();
-        int delete_block();
+        void delete_block();
     };
     
     struct super_block: public block{
-        int block_size;
         int block_num;
-        int final_block_offset;
-        char path[256];
+        int first_empty_block_offset;
+        int first_used_block_offset;
+        int final_used_block_offset;
+
+        super_block(shared_ptr<fstream> _file_stream, int _block_size);
+        shared_ptr<block> new_block();
     };
 
     struct used_block: public block{
