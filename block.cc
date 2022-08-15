5c01321aa04a495e2c421eba6b10c79a2f06790c
yonde927 <svyuta1202@gmail.com>
2022-08-15 20:36:33 +0900

    block

--- block.cc
+++ block.cc
@@ -1,79 +1,44 @@
 #include "block.h"
 
 namespace fique{
-    super_block::super_block(string _path, int _block_size){
-        block_num = 0; 
-        first_empty_block_offset = block_size; 
-        final_empty_block_offset = block_size; 
-        first_used_block_offset = -1; 
-        final_used_block_offset = -1;
-        //ファイルへの書き込み
-        file_stream->seekg(0);
-        file_stream->write((const char*)&block_num, sizeof(int));
-        file_stream->write((const char*)&first_empty_block_offset, sizeof(int));
-        file_stream->write((const char*)&final_empty_block_offset, sizeof(int));
-        file_stream->write((const char*)&first_used_block_offset, sizeof(int));
-        file_stream->write((const char*)&final_used_block_offset, sizeof(int));
+    block::block(shared_ptr<fstream> _file_stream, int _this_offset, int _block_size){
+        file_stream = _file_stream;
+        this_offset = _this_offset;
+        block_size = _block_size;
     }
 
-    shared_ptr<block> super_block::new_block(){
-        int new_block_offset{0};
-        //空ブロックが存在しない場合、使用済みオフセットに1ブロック分のサイズを加算したオフセットを作成する
-        if(first_empty_block_offset == -1){
-            new_block_offset = final_used_block_offset + block_size;
-        }else{
-            new_block_offset = first_empty_block_offset;
-            auto eblock = std::make_shared<block>();
-            first_empty_block_offset = 
-        }
-    }
-
-    block::block(shared_ptr<fstream> _file_stream, int _offset, int _block_size){
-        file_stream = _file_stream;
-        this_offset = _offset;
-        block_size = _block_size;
-        file_stream->seekg(_offset);
-        file_stream->read((char*)&prev_offset, sizeof(int));
-        file_stream->read((char*)&next_offset, sizeof(int));
-    }
-    
-    block::block(shared_ptr<fstream> _file_stream, int _offset, int _block_size, int _prev_offset, int _next_offset){
-        file_stream = _file_stream;
-        this_offset = _offset;
-        block_size = _block_size;
-        prev_offset = _prev_offset;
-        next_offset = _next_offset;
-        //ファイルへの書き込み
+    int block::get_next_offset(){
+        int offset{0};
         file_stream->seekg(this_offset);
-        file_stream->write((const char*)&block_size, sizeof(int));
-        file_stream->write((const char*)&prev_offset, sizeof(int));
-        file_stream->write((const char*)&next_offset, sizeof(int));
+        file_stream->read((char*)&offset, sizeof(int)); 
+        return offset;
     }
 
-    shared_ptr<block> block::prev_block(){
-        if(prev_offset == -1){
-            shared_ptr<block> pb(nullptr);
-            return pb;
-        }
-        return std::make_shared<block>(file_stream, prev_offset, block_size);
+    int block::set_next_offset(int offset){
+        file_stream->seekg(this_offset);
+        file_stream->write((const char*)&offset, sizeof(int)); 
+        return offset;
+    }
+
+    int block::get_prev_offset(){
+        int offset{0};
+        file_stream->seekg(this_offset + sizeof(int));
+        file_stream->read((char*)&offset, sizeof(int)); 
+        return offset;
+    }
+
+    int block::set_prev_offset(int offset){
+        file_stream->seekg(this_offset + sizeof(int));
+        file_stream->write((const char*)&offset, sizeof(int)); 
+        return offset;
     }
 
     shared_ptr<block> block::next_block(){
+        int next_offset = get_next_offset();
         if(next_offset == -1){
-            shared_ptr<block> pb(nullptr);
-            return pb;
-        }
-        return std::make_shared<block>(file_stream, next_offset, block_size);
-    }
-
-    void block::delete_block(){
-        file_stream->seekg(prev_offset + sizeof(int));
-        file_stream->write((const char*)&next_offset, sizeof(int));
-        //削除ブロックのオフセットが使用済みブロックの手前にある場合、空ブロックとして登録する。
-        if(this_offset < final_used_block_offset){
-            //final
-            file_stream->seekg(sizeof(int));
+            return shared_ptr<block>();
+        }else{
+            return std::make_shared<block>(file_stream, next_offset, block_size);
         }
     }
-
 }
--- block.h
+++ block.h
@@ -15,21 +15,46 @@ namespace fique{
         int block_size;
         shared_ptr<fstream> file_stream;
 
-        block(shared_ptr<fstream> _file_stream, _this_offset, _block_size);
+        block(shared_ptr<fstream> _file_stream, int _this_offset, int _block_size);
         int get_next_offset();
-        int set_next_offset();
+        int set_next_offset(int offset);
         int get_prev_offset();
-        int set_prev_offset();
+        int set_prev_offset(int offset);
         int get_data_offset();
-        int write_buffer(const void* buffer, int size);
-        int read_buffer(void* buffer, int size);
+        template<class T>
+        void write_buffer(const T& buffer){
+            int writable_size = block_size - sizeof(int) * 2;
+            int type_size = sizeof(T);
+            if(type_size > writable_size){
+                std::cout << "write type size is bigger than block writable size" << std::endl;
+                return;
+            }
+            file_stream->seekg(this_offset + sizeof(int) * 2);
+            file_stream->write((const char*)&buffer, type_size);
+        }
+        template<class T>
+        void read_buffer(T& buffer){
+            int writable_size = block_size - sizeof(int) * 2;
+            int type_size = sizeof(T);
+            if(type_size > writable_size){
+                std::cout << "read type size is bigger than block writable size" << std::endl;
+                return;
+            }
+            file_stream->seekg(this_offset + sizeof(int) * 2);
+            file_stream->read((char*)&buffer, type_size);
+        }
+        shared_ptr<block> next_block();
+        shared_ptr<block> prev_block();
     };
     //file_queueはブロックの走査・参照・追加・削除をする。使用済みと空ブロックの管理を実行し、super_blockに保存する
     struct file_list {
         shared_ptr<fstream> file_stream;
+
         file_list(string path, int block_size);
         shared_ptr<block> new_block();
+        void delete_block(int block_offset);
         shared_ptr<block> first_block();
+        shared_ptr<block> final_block();
     };
 }
 
