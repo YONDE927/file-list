da5bb7b24674a72b1b9cbd96a2ff93758b361d5d
yonde927 <svyuta1202@gmail.com>
2022-08-20 13:55:18 +0900

    iter

--- test.cc
+++ test.cc
@@ -20,22 +20,30 @@ int main(){
 
     list.reset();
 
+    obj rbuffer{0};
+
     auto block_ptr1 = list.new_back_block();
     obj buffer1{1,2,3};
-    obj rbuffer1{0};
     block_ptr1->write_buffer<obj>(buffer1);
-    //fili::show_fili(path, 32);
-    block_ptr1->read_buffer<obj>(rbuffer1);
-    cout << endl << rbuffer1.a << " " << rbuffer1.b << " " << rbuffer1.c << endl;
-
-    cout << endl;
+    block_ptr1->read_buffer<obj>(rbuffer);
+    cout << rbuffer.a << " " << rbuffer.b << " " << rbuffer.c << endl;
 
     auto block_ptr2 = list.new_back_block();
     obj buffer2{100,200,300};
     block_ptr2->write_buffer<obj>(buffer2);
-    //fili::show_fili(path, 32);
-    //auto block_ptr3 = list.new_front_block();
-    //list.delete_block(block_ptr2);
-    //auto block_ptr4 = list.new_back_block();
+    
+    auto block_ptr3 = list.new_back_block();
+    obj buffer3{1000,2000,3000};
+    block_ptr3->write_buffer<obj>(buffer3);
+
+    auto blo = list.first_block();
+    int cnt{0};
+    while(blo != nullptr){
+        blo->read_buffer<obj>(rbuffer);
+        cout << "index: " << cnt++ << endl;
+        cout << rbuffer.a << " " << rbuffer.b << " " << rbuffer.c << endl;
+        blo = blo->next_block();
+    }
+
     return 0;
 }
